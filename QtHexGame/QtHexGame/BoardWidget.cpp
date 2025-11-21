d217b313cf9158475b6f92b53ec4a7b08ce16795
Nobuhide Tsuda <ntsuda@master.email.ne.jp>
2025-11-22 08:03:57 +0900

    0002 ◎ C 11/22 11/22 0.001│ToDo│盤面表示用 BoardWidget 設置

---
+++ QtHexGame/QtHexGame/BoardWidget.cpp
@@ -0,0 +1,25 @@
+﻿#include <QPainter>
+#include "BoardWidget.h"
+
+const int N_HORZ = 4;
+
+void BoardWidget::paintEvent(QPaintEvent* event)
+{
+	double ww = width();
+    double wh = height();
+
+    QPainter painter(this);
+    // アンチエイリアシング（滑らかに描画）
+    painter.setRenderHint(QPainter::Antialiasing);
+    // ペンの設定（線のカラー・太さ・スタイル）
+    QPen pen;
+    pen.setWidth(1);                    // 線幅 1px
+    pen.setColor(Qt::black);            // 黒線
+    pen.setCapStyle(Qt::RoundCap);      // 線の端を丸く
+    pen.setJoinStyle(Qt::RoundJoin);    // 折れ線の接続部も丸く
+    painter.setPen(pen);
+    // ブラシの設定（塗りつぶし）
+    painter.setBrush(QBrush(Qt::yellow));  // 黄色で塗りつぶし
+
+    painter.drawLine(QPoint(0, 0), QPoint(ww, wh));
+}
---
+++ QtHexGame/QtHexGame/BoardWidget.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+//#include "C:\Qt\6.10.0\msvc2022_64\include\QtWidgets\qwidget.h"
+#include <qwidget.h>
+
+class BoardWidget :
+    public QWidget
+{
+public:
+    BoardWidget(QWidget *parent)
+    	: QWidget(parent)
+	{
+	}
+
+protected:
+    void paintEvent(QPaintEvent* event) override;
+
+};
+
